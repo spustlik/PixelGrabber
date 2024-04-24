d382cc3863770796e3917af4ea8112ba41aa66a7
Jan Stuchlik <j.stuchlik@outlook.cz>
2024-04-24 16:25:53 +0200

    fix

--- WpfGrabber/ViewParts/Binary8BitViewPart.xaml.cs
+++ WpfGrabber/ViewParts/Binary8BitViewPart.xaml.cs
@@ -39,7 +39,6 @@ namespace WpfGrabber.ViewParts
 
     public partial class Binary8BitViewPart : Binary8BitViewPartBase
     {
-        private ShellVm shellVm;
         public Binary8BitViewPart()
         {
             DataContext = new Binary8BitVM();
@@ -56,23 +55,23 @@ namespace WpfGrabber.ViewParts
 
         protected override void OnShowData()
         {
-            if (shellVm.DataLength == 0)
+            if (ShellVm.DataLength == 0)
                 return;
-            var reader = new BitReader(shellVm.Data);
-            reader.BytePosition = shellVm.Offset;
+            var reader = new BitReader(ShellVm.Data);
+            reader.BytePosition = ShellVm.Offset;
             reader.ReverseByte = ViewModel.Reversed;
             var w = ViewModel.Width;
            
             var total_w = this.GetFirstValid(imageBorder.ActualWidth, imageBorder.Width, Width, 100);
             var total_h = this.GetFirstValid(imageBorder.ActualHeight, imageBorder.Height, Height,200);
-            total_h = (int)(total_h / shellVm.Zoom);
+            total_h = (int)(total_h / ShellVm.Zoom);
             //log.Text += $"H:{total_h},W:{total_w}, w={w}\n";
 
             var space = w < 16 ? 4 : 10;
             var bir = new BitImageReader();
             BitmapSource bmp = bir.ReadBitmap(reader, total_w, total_h, w, space);
             image.Source = bmp;
-            image.RenderTransform = new ScaleTransform(shellVm.Zoom, shellVm.Zoom);
+            image.RenderTransform = new ScaleTransform(ShellVm.Zoom, ShellVm.Zoom);
 
         }
 
@@ -81,7 +80,7 @@ namespace WpfGrabber.ViewParts
             var dlg = new SaveFileDialog();
             dlg.DefaultExt = "png";
             dlg.OverwritePrompt = true;
-            dlg.FileName = $"{Path.GetFileName(shellVm.FileName)}-{shellVm.Offset}-{shellVm.Offset:X4}-{ViewModel.Width}";
+            dlg.FileName = $"{Path.GetFileName(ShellVm.FileName)}-{ShellVm.Offset}-{ShellVm.Offset:X4}-{ViewModel.Width}";
             if (dlg.ShowDialog(App.Current.MainWindow) != true)
                 return;
             ((BitmapSource)image.Source).SaveToPngFile(dlg.FileName);        
@@ -92,11 +91,11 @@ namespace WpfGrabber.ViewParts
             var dlg = new SaveFileDialog();
             dlg.DefaultExt = "png";
             dlg.OverwritePrompt = true;
-            dlg.FileName = $"{Path.GetFileName(shellVm.FileName)}-{shellVm.Offset}-{shellVm.Offset:X4}-{ViewModel.Width}.data.png";
+            dlg.FileName = $"{Path.GetFileName(ShellVm.FileName)}-{ShellVm.Offset}-{ShellVm.Offset:X4}-{ViewModel.Width}.data.png";
             var bir = new BitImageReader();
-            var bmp2 = bir.ReadBitmap(new BitReader(shellVm.Data) { BytePosition = shellVm.Offset },
+            var bmp2 = bir.ReadBitmap(new BitReader(ShellVm.Data) { BytePosition = ShellVm.Offset },
                 ViewModel.Width,
-                (shellVm.DataLength - shellVm.Offset) / (ViewModel.Width / 8),
+                (ShellVm.DataLength - ShellVm.Offset) / (ViewModel.Width / 8),
                 ViewModel.Width, 10);
             bmp2.SaveToPngFile(dlg.FileName);
         }
