02b3a5d820de12647064c2702bf58aa2f73d12a4
Jan Stuchlik <j.stuchlik@outlook.cz>
2024-04-24 16:22:13 +0200

    fonts

---
+++ UnitTests/FontTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using WpfGrabber;
+using WpfGrabber.Readers;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class FontTests
+    {
+        [TestMethod]
+        public void TestFlip()
+        {
+            { 
+                var r = BitReader.GetFlippedX(0b11110000);
+                Assert.AreEqual(r, 0b00001111);
+            }
+            {
+                var r = BitReader.GetFlippedX(0b10110000);
+                Assert.AreEqual(r, 0b00001101);
+            }
+            {
+                var r = BitReader.GetFlippedX(0b00001111);
+                Assert.AreEqual(r, 0b11110000);
+            }
+            {
+                var r = BitReader.GetFlippedX(0b00000001);
+                Assert.AreEqual(r, 0b10000000);
+            }
+        }
+    }
+}
