63dd272c38a243804d23e982635dc0203ddb0678
Jan Stuchlik <j.stuchlik@outlook.cz>
2024-05-17 16:50:56 +0200

    indexed bitmap with palette

--- WpfGrabber/Data/GraphicsWpf.cs
+++ WpfGrabber/Data/GraphicsWpf.cs
@@ -23,13 +23,46 @@ namespace WpfGrabber.Data
 
         public static ByteBitmapRgba ToRgba(this BitmapSource src)
         {
-            int width = (int)src.Width;
-            int height = (int)src.Height;
+            if (src.Format == PixelFormats.Indexed8)
+                return FromIndexed(src);
+            if (src.Format == PixelFormats.Bgr32)
+                return FromBGRA(src);
+            throw new NotImplementedException($"Bitmap format {src.Format} not implemented");
+        }
+
+        private static ByteBitmapRgba FromBGRA(BitmapSource src)
+        {
+            int width = src.PixelWidth;
+            int height = src.PixelHeight;
+            var bytesPerPx = src.Format.BitsPerPixel / 8;
             var pixels = new uint[width * height];
-            src.CopyPixels(pixels, stride: width * 4, 0);
+            src.CopyPixels(pixels, stride: width * bytesPerPx, 0);
             var r = new ByteBitmapRgba(width, height);
-            //r.Data = pixels;
             Array.Copy(pixels, r.Data, pixels.Length);
+            return r;
+        }
+
+        private static ByteBitmapRgba FromIndexed(BitmapSource src)
+        {
+            if (src.Palette == null)
+                throw new NotImplementedException($"Bitmap format {src.Format} without palette not implemented");
+            if (src.Palette.Colors.Count != 256)
+                throw new FormatException($"Palette should have 256 colors ({src.Palette.Colors.Count})");
+            int width = src.PixelWidth;
+            int height = src.PixelHeight;
+            var r = new ByteBitmapRgba(width, height);
+            var pixels = new byte[width * height];
+            src.CopyPixels(pixels, stride: width, 0);
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    var b = pixels[x + y * width];
+                    var cc = src.Palette.Colors[b];
+                    var c = ByteColor.FromRgba(cc.R, cc.G, cc.B, cc.A);
+                    r.SetPixel(x, y, c);
+                }
+            }
             return r;
         }
     }
